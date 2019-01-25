3b422da150711b11297338fb41b8f82ff4a7ba32
paulkatipunan <paul.katipunan@synergy88digital.com>
2019-01-25 12:53:49 +0800

    updates

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Events\NewLogin;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,13 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::post('/client', function (Request $request) {
+
+	broadcast(new NewLogin($request->all()))->toOthers();
+
+	return response(['success'], 200);	
+
 });
